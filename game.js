62e1e4b21066c385d08387297e323fbad363501a
gurbanovali <aligurbanov95@mail.ru>
2021-02-22 18:07:31 +0300

    add first result chapter4

--- game.js
+++ game.js
@@ -2,16 +2,18 @@ var width = 1280,
 	height = 720,
 	loader = PIXI.loader,
 	resources = loader.resources,
-	renderer = new PIXI.autoDetectRenderer(width, height, {view: document.getElementById("canvas")});
-	stage = new PIXI.Container();
-	stageReel = new PIXI.Container();
-	stageReelstrip = new PIXI.Container();
-	
-	// Sprite = PIXI.Sprite,
+	renderer = new PIXI.autoDetectRenderer(width, height, {
+		view: document.getElementById("canvas")
+	}),
+	stage = new PIXI.Container(),
+	reelPanelOne = new PIXI.Container(),
+	stateBtn = null,
 	ticker = null,
 	reelPanel = null,
 	reel1 = null,
-	reelstrip1 = [
+	reel2 = null,
+	reel3 = null,
+	reelPaneltrip1 = [
 		0, //Seven
 		1, //Bar
 		2, //Melon
@@ -20,6 +22,7 @@ var width = 1280,
 		5, //Orange
 		6, //Lemon
 		7, //Cherry
+		8, //Star
 		0, //Seven
 		2, //Melon
 		4, //Plum
@@ -27,54 +30,288 @@ var width = 1280,
 		1, //Bar
 		3, //Grapes
 		5, //Orange
-		7  //Cherry
-	];
-	
+		7, //Cherry
+		8 //Star
+	],
+	reelPaneltrip2 = [
+		0, //Seven
+		1, //Bar
+		1, //Bar
+		2, //Melon
+		3, //Grapes
+		4, //Plum
+		6, //Lemon
+		6, //Lemon
+		7, //Cherry
+		8, //Star
+		0, //Seven
+		2, //Melon
+		4, //Plum
+	],
+	reelPaneltrip3 = [
+		0, //Seven
+		0, //Seven
+		8, //Star
+		3, //Grapes
+		4, //Plum
+		5, //Orange
+		6, //Lemon
+		7, //Cherry
+		7, //Cherry
+		2, //Melon
+		4, //Plum
+		2, //Melon
+		6, //Lemon
+		1, //Bar
+		1, //Bar
+		3, //Grapes
+		5, //Orange
+		7, //Cherry
+		7, //Cherry
+		2 //Melon
+	],
+	score = [40, 20, 16, 16, 2, 2, 2, 2, 50],
+	wfd;
 loader
-	.add('bgr', 'images/bgr.jpg')
-	.add('reels', 'images/reels.png')	
-	.add('seven', 'images/gsym_0.png')
-	.add('bar', 'images/gsym_1.png')
-	.add('melon', 'images/gsym_2.png')
-	.add('grapes', 'images/gsym_3.png')
-	.add('plum', 'images/gsym_4.png')
-	.add('orange', 'images/gsym_5.png')
-	.add('lemon', 'images/gsym_6.png')
-	.add('cherry', 'images/gsym_7.png')
+	.add('bgr', 'images/bgr.png')
+	.add('reelPanel', 'images/reels.png')
+	.add('gsym', 'images/gsym.json')
+	.add('btn_pressed', 'images/btn_pressed.png')
+	.add('ico_play', 'images/ico_play.png')
+	.add('btn', 'images/btn.png')
+	.add('paytable', 'images/paytable.png')
+	.add('logo', 'images/logo.png')
+	.add('wfd', 'images/wfd.png')
 	.load(onAssetsLoaded);
 
-function onAssetsLoaded() 
-{
+function onAssetsLoaded() {
 	init();
 }
-				
-function init()
-{
-	var bgr = new PIXI.Sprite(resources.bgr.texture);
-	var reels = new PIXI.Sprite(resources.reels.texture)
-	var seven = new PIXI.Sprite(resources.seven.texture)
-	var bar = new PIXI.Sprite(resources.bar.texture)
-	var melon = new PIXI.Sprite(resources.melon.texture)
-	
+
+function init() {
+	const bgr = new PIXI.Sprite(resources.bgr.texture);
 	stage.addChild(bgr);
-	stage.addChild(stageReel)
-	stageReel.addChild(reels).position.set(512, 96)
-	stageReel.addChild(stageReelstrip)
-	stageReelstrip.addChild(seven).position.set(522,106)
-	stageReelstrip.addChild(bar).position.set(522,274)
-	stageReelstrip.addChild(melon).position.set(522,424)
-		
-	//TODO init the reelPanel (Sprite) and add it at the center of the stage
-		
-	//TODO init the reel1 (see reel.js) and position it at the center of the stage
-		
+
+	reelPanel = new PIXI.Sprite(resources.reelPanel.texture);
+	reelPanelOne.addChild(reelPanel);
+
+	reelPanelOne.position.set(renderer.width / 2 - reelPanel.width / 2, renderer.height / 2 - reelPanel.height / 2);
+
+	var paytableSprite = new PIXI.Sprite(resources.paytable.texture),
+		paytable = new Paytable(paytableSprite, score);
+	paytable.position.set(25, renderer.height / 2 - paytableSprite.height / 2);
+	paytable.init();
+	stage.addChild(paytable)
+
+	reel1 = new Reel(reelPaneltrip1, 9, 1);
+	reel1.init();
+
+	reel1.position.set(10, 10);
+	reelPanelOne.addChild(reel1)
+
+	reel2 = new Reel(reelPaneltrip2, 9, 2);
+	reel2.init();
+
+	reel2.position.set(218, 10);
+	reelPanelOne.addChild(reel2)
+
+	reel3 = new Reel(reelPaneltrip3, 9, 3);
+	reel3.init();
+
+	reel3.position.set(426, 10);
+	reelPanelOne.addChild(reel3);
+
+	var logo = new PIXI.Sprite(resources.logo.texture);
+	logo.position.set(renderer.width / 2 - logo.width / 2, 20);
+	//logo.visible = false;
+	stage.addChild(logo);
+
+	wfd = new PIXI.Sprite(resources.wfd.texture);
+	wfd.position.set(renderer.width / 2 - wfd.width / 2, 20);
+	wfd.visible = false;
+	stage.addChild(wfd)
+
+	var icoPlay = new PIXI.Sprite(resources.ico_play.texture),
+		textureUp = resources.btn.texture,
+		textureDown = resources.btn_pressed.texture,
+		button = new Button(icoPlay, textureUp, textureDown, pressBtn, releaseBtn);
+	button.position.set(1050, 300);
+	button.init();
+	stage.addChild(button);
+
+
+	stage.addChild(reelPanelOne);
 	ticker = PIXI.ticker.shared;
 	ticker.add(render);
+	render();
 }
 
-function render()
-{
-	//TODO call the spin function of the reel
-	
+function pressBtn() {
+	this.button.texture = this.textureDown;
+	if (stateBtn === null) {
+		ticker.add(stop);
+		reel1.getPosition(0);
+		reel2.getPosition(0);
+		reel3.getPosition(0);
+		wfd.visible = false;
+		reel1.arrSprits[1].gotoAndStop(0)
+		reel2.arrSprits[1].gotoAndStop(0)
+		reel3.arrSprits[1].gotoAndStop(0)
+		reel1.arrSprits[2].gotoAndStop(0)
+		reel2.arrSprits[2].gotoAndStop(0)
+		reel3.arrSprits[2].gotoAndStop(0)
+		ticker.add(getStop);
+		reel1.vy = 30;
+		reel2.vy = 30;
+		reel3.vy = 30;
+		stateBtn = 1;
+	} else if (stateBtn === 1 && reel1.state == null && reel2.state == null && reel3.state == null) {
+		reel1.vy = 30;
+		reel2.vy = 30;
+		reel3.vy = 30;
+		stateBtn = null;
+	} else if (stateBtn === 1 && reel1.state == null && reel2.state == null) {
+		reel1.vy = 30;
+		reel2.vy = 30;
+		stateBtn = null;
+	} else if (stateBtn === 1 && reel1.state == null && reel3.state == null) {
+		reel1.vy = 30;
+		reel3.vy = 30;
+		stateBtn = null;
+	} else if (stateBtn === 1 && reel2.state == null && reel3.state == null) {
+		reel2.vy = 30;
+		reel3.vy = 30;
+		stateBtn = null;
+	} else if (stateBtn === 1 && reel1.state == null) {
+		reel1.vy = 30;
+		stateBtn = null;
+	} else if (stateBtn === 1 && reel2.state == null) {
+		reel2.vy = 30;
+		stateBtn = null;
+	} else if (stateBtn === 1 && reel3.state == null) {
+		reel3.vy = 30;
+		stateBtn = null;
+	};
+}
+
+function releaseBtn() {
+	this.button.texture = this.textureUp;
+};
+
+
+function getStop() {
+	reel1.stop();
+	reel2.stop();
+	reel3.stop();
+};
+
+function stop() {
+	if (reel1.state == 2 && reel2.state == 2 && reel3.state == 2) {
+		ticker.remove(getStop);
+		reel1.state = null;
+		reel2.state = null;
+		reel3.state = null;
+		stateBtn = null;
+		winPosition()
+		ticker.remove(winPosition);
+		ticker.remove(stop);
+	}
+}
+
+function render() {
 	renderer.render(stage);
 };
+
+function winPosition() {
+
+	if (reel1.arrSprits[2].texture === reel2.arrSprits[2].texture && reel2.arrSprits[2].texture === reel3.arrSprits[2].texture) {
+		scores(reel1.arrSprits[2].textures)
+		wfd.visible = true;
+		reel1.arrSprits[2].play();
+		reel2.arrSprits[2].play();
+		reel3.arrSprits[2].play();
+	}
+
+	if (reel1.arrSprits[1].texture === reel2.arrSprits[1].texture && reel2.arrSprits[1].texture === reel3.arrSprits[1].texture) {
+		scores(reel1.arrSprits[1].textures)
+		reel1.arrSprits[1].play();
+		reel2.arrSprits[1].play();
+		reel3.arrSprits[1].play();
+	}
+
+	if (reel1.arrSprits[3].texture === reel2.arrSprits[3].texture && reel2.arrSprits[3].texture === reel3.arrSprits[3].texture) {
+		scores(reel1.arrSprits[3].textures)
+		reel1.arrSprits[3].play();
+		reel2.arrSprits[3].play();
+		reel3.arrSprits[3].play();
+	}
+
+	if (reel1.arrSprits[1].texture === reel2.arrSprits[2].texture && reel2.arrSprits[2].texture === reel3.arrSprits[3].texture) {
+		scores(reel1.arrSprits[1].textures)
+		reel1.arrSprits[1].play();
+		reel2.arrSprits[2].play();
+		reel3.arrSprits[3].play();
+	}
+
+	if (reel1.arrSprits[3].texture === reel2.arrSprits[2].texture && reel2.arrSprits[2].texture === reel3.arrSprits[1].texture) {
+		scores(reel1.arrSprits[3].textures)
+		reel1.arrSprits[3].play();
+		reel2.arrSprits[2].play();
+		reel3.arrSprits[1].play();
+	}
+
+}
+
+function scores(text) {
+	var indexArr = reel1.arrTextures.indexOf(text)
+	var getScore = score.find((item, i) => i === indexArr)
+
+	var style = {
+        fontSize: "48px",
+        fontWeight: "bold",
+        fill: "white"
+    };
+
+	var countingText = new PIXI.Text('0', style);
+	var count = 0;
+	ticker.add(animate)
+
+	// if(count == getScore) {
+	// 	ticker.remove(animate)
+	// 	ticker.add(animate2)
+	// }
+
+	function animate() {
+
+		if(count <= getScore) {
+			count += 0.005;
+			// count -= 0.005;
+
+		}
+		
+        countingText.text = Math.floor(count);
+		countingText.position.set(renderer.width / 2 , 50);
+		countingText.anchor.set(0.5, 0.5);
+		stage.addChild(countingText);
+
+        requestAnimationFrame(animate);
+
+    }
+	if(count == getScore ){
+		console.log(true)
+	}
+	// function animate2() {
+
+	// 	if(count == getScore) count -= 0.005;
+
+		
+		
+    //     countingText.text = Math.floor(count);
+	// 	countingText.position.set(renderer.width / 2 , 50);
+	// 	countingText.anchor.set(0.5, 0.5);
+	// 	stage.addChild(countingText);
+
+    //     requestAnimationFrame(animate2);
+
+    // }
+}
--- reel.js
+++ reel.js
@@ -1,34 +1,90 @@
-function Reel(strip)
-{
+function Reel(strip, n, numImg) {
 	PIXI.Container.call(this);
-	
 	this.reelstrip = strip;
+	this.numImg = numImg;
+	this.arrTextures = [];
+	this.arrSprits = [];
+	this.vy = null;
+	this.stopElement = null;
+	this.state = null;
+	this.bufferArr = [];
+	this.heightSprite = 168;
+	this.widthSprite = 198;
+	this.value = null;
 }
+
 Reel.prototype = Object.create(PIXI.Container.prototype);
 Reel.prototype.constructor = Reel;
 
-Reel.prototype.init = function()
-{
-	/*
-	 * TODO add all necessary elements of the reel (sprites, mask) here
-	 *
-	 * Add 4 symbols (sprites) and position them on the reel 
-	 * The first symbol is positioned outside of the reel
-	 * The second symbol is the first visible symbol followed by the other symbols
-	 * The mask is covering the visible part of the reel
-	 */
+Reel.prototype.init = function () {
+	for (i = 0; i < 9; i++) {
+		let currentArr = [];
+		for (j = 0; j < 11; j++) {
+			currentArr.push(resources["gsym"].textures["gsym_" + i + "_" + j]);
+		}
+		this.arrTextures.push(currentArr)
+	}
+
+	for (i = 0; i < 4; i++) {
+		this.arrSprits[i] = new PIXI.extras.AnimatedSprite(this.arrTextures[this.reelstrip[i]]);
+		this.arrSprits[i].animationSpeed = 0.2;
+		this.arrSprits[i].anchor.set(0.5, 0.5);
+		this.arrSprits[i].y = this.heightSprite * i - this.heightSprite / 2;
+		this.arrSprits[i].x = this.widthSprite / 2;
+		this.addChild(this.arrSprits[i]);
+	}
+
+	//mask
+	let rectangle = new PIXI.Graphics();
+	rectangle.drawRect(0, 0, reelPanel.width - 20, reelPanel.height - 20);
+	this.mask = rectangle;
+	this.addChild(rectangle);
 };
 
-Reel.prototype.spin = function()
-{
-	/*
-	 * TODO implement the spin functionality here
-	 * 
-	 * Move all symbols downwards 
-	 * If a symbol is out of the reel
-	 * position it above the reel (where first symbol is positioned at the start) 
-	 * and change the symbol according to the reelstrip
-	 */
-};
+Reel.prototype.spin = function (boolean) {
+	for (i = 0; i < this.arrSprits.length; i++) {
+		this.arrSprits[i].y += this.vy;
+	}
+	if (boolean && this.arrSprits[0].y > this.heightSprite / 2) {
+		this.arrSprits.unshift(this.arrSprits.pop());
+		this.arrSprits[0].y = this.arrSprits[1].y - this.heightSprite;
+		if (this.bufferArr.length > 0) {
+			this.arrSprits[0].textures = this.arrTextures[this.bufferArr.pop()];
+			this.arrSprits[0].updateTexture()
+		}
+	}
+}
 
-//TODO add additional used functions of the reel element here
+Reel.prototype.stop = function () {
+	if (this.bufferArr.length === 0 && this.arrSprits[1].y > reelPanel.height / 2 - 139 && this.arrSprits[1].y <= reelPanel.height / 2 + 30 && this.vy > 1) {
+		this.state = 1;
+		this.vy = 30;
+		this.spin(false);
+	} else if (this.bufferArr.length === 0 && this.arrSprits[1].y >= reelPanel.height / 2 + 30 && this.vy > 0) {
+		this.vy = -1;
+		this.spin(false);
+	} else if (this.bufferArr.length === 0 && this.arrSprits[2].y === reelPanel.height / 2 - 10 && this.vy < 0) {
+		this.vy = 0;
+		this.state = 2;
+	} else {
+		this.spin(true);
+	}
+}
+
+Reel.prototype.getPosition = function (n) {
+	// if you want to make three identical elements
+	this.stopElement = n
+	let currentEl = this.stopElement
+	//let currentEl = Math.floor(Math.random() * ((this.reelstrip.length - 1) + 1));
+	currentEl--;
+	for (i = 0; i < this.numImg * 5 + 3; i++) {
+		if (currentEl > this.reelstrip.length - 1) {
+			currentEl -= this.reelstrip.length
+		}
+		if (currentEl < 0) {
+			currentEl += this.reelstrip.length
+		}
+		this.bufferArr.push(this.reelstrip[currentEl])
+		currentEl++;
+	}
+}
